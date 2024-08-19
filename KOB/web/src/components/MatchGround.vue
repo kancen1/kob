7f8f5de85cce95a1d45a470c9eed43f8f84fb5f7
阚 尘 <2643850706@qq.com>
2024-08-19 19:29:44 +0800

    匹配跳转页面延迟两秒钟出错，同步web端

--- KOB/web/src/components/MatchGround.vue
+++ KOB/web/src/components/MatchGround.vue
@@ -1,91 +1,144 @@
 <template>
-    <div class="matchground">
-      <div class="row">
-        <div class="col-6">
-            <div class="user-photo">
-                <img :src="$store.state.user.photo" alt="">
-            </div>
-
-            <div class="user-username">
-                {{ $store.state.user.username }}
-            </div>
+  <div class="matchground">
+    <div class="row">
+      <div class="col-4">
+        <div class="user-photo">
+          <img :src="$store.state.user.photo" alt="" />
         </div>
-        <div class="col-6">
-            <div class="user-photo">
-                <img :src="$store.state.pk.opponent_photo" alt="">
-            </div>
-
-            <div class="user-username">
-                {{ $store.state.pk.opponent_username }}
-            </div>
+        <div class="user-username">
+          {{ $store.state.user.username }}
         </div>
       </div>
-      <div class="col-12" style="text-align: center; padding-top: 15vh;">
-        <button @click="click_match_btn" type="button" class="btn btn-info btn-lg">{{ match_btn_info }}</button>
+      <div class="col-4">
+        <div class="user-select-bot">
+          <select
+            v-model="select_bot"
+            class="form-select"
+            aria-label="Default select example"
+          >
+            <option value="-1" selected>亲自出马</option>
+            <option v-for="bot in bots" :key="bot.id" :value="bot.id">
+              {{ bot.title }}
+            </option>
+          </select>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="user-photo">
+          <img :src="$store.state.pk.opponent_photo" alt="" />
+        </div>
+
+        <div class="user-username">
+          {{ $store.state.pk.opponent_username }}
+        </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-import { ref } from 'vue';
-import { useStore } from 'vuex';
+    <div class="col-12" style="text-align: center; padding-top: 15vh">
+      <button
+        @click="click_match_btn"
+        type="button"
+        class="btn btn-info btn-lg"
+      >
+        {{ match_btn_info }}
+      </button>
+    </div>
+  </div>
+</template>
 
-  
-  export default {
-      name: 'MatchGround',
-      setup() {
-        const store = useStore();
-        let match_btn_info = ref('开始匹配');
-        
-        const click_match_btn = () => {
-          if (match_btn_info.value === '开始匹配') {
-            match_btn_info.value = '取消匹配';
-            // 开始匹配的话向后端发送一个请求 send API可以向后端通信
-            store.state.pk.socket.send(JSON.stringify({
-              // stringify将js对象转换为字符串
-              // 表示开始匹配
-              event: 'start-matching',
-            }));
-          } else {
-            match_btn_info.value = '开始匹配';
-            // 取消匹配的话发送请求
-            store.state.pk.socket.send(JSON.stringify({
-              event: 'stop-matching',
-          }));
-        }
+<script>
+import { ref } from "vue";
+import { useStore } from "vuex";
+import $ from "jquery";
+
+export default {
+  name: "MatchGround",
+  setup() {
+    const store = useStore();
+    let match_btn_info = ref("开始匹配");
+    let bots = ref("");
+    let select_bot = ref("-1");
+
+    const click_match_btn = () => {
+      if (match_btn_info.value === "开始匹配") {
+        match_btn_info.value = "取消匹配";
+        // 开始匹配的话向后端发送一个请求 send API可以向后端通信
+        store.state.pk.socket.send(
+          JSON.stringify({
+            // stringify将js对象转换为字符串
+            // 表示开始匹配
+            event: "start-matching",
+            bot_id: select_bot.value,
+          })
+        );
+      } else {
+        match_btn_info.value = "开始匹配";
+        // 取消匹配的话发送请求
+        store.state.pk.socket.send(
+          JSON.stringify({
+            event: "stop-matching",
+          })
+        );
       }
-      return {
-          match_btn_info,
-          click_match_btn,
-        }
-    }
-  }
-  </script>
-  
-  <style scoped>
-  div.matchground {
-      width: 60vw;
-      height: 70vh;
-      margin: 40px auto;
-      background-color: rgba(50, 50, 50, 0.5);
-  }
+    };
 
-  div.user-photo {
-    text-align: center;
-    padding-top: 10vh;
-  }
+    const refresh_bots = () => {
+      $.ajax({
+        url: "http://localhost:3000/user/bot/getlist/",
+        type: "get",
+        headers: {
+          Authorization: "Bearer " + store.state.user.token,
+        },
+        success(resp) {
+          bots.value = resp;
+        },
+      });
+    };
 
-  div.user-photo>img {
-    border-radius: 50%;
-    width: 20vh;
-  }
+    // 当组件挂载的时候，调用refresh_bots函数，从云端获取bot
+    refresh_bots();
 
-  div.user-username {
-    text-align: center;
-    font-size: 24px;
-    font-weight: 600;
-    color: white;
-    padding-top: 2vh;
-  }
+    return {
+      match_btn_info,
+      click_match_btn,
+      bots,
+      select_bot,
+    };
+  },
+};
+</script>
 
-  </style>
+<style scoped>
+div.matchground {
+  width: 60vw;
+  height: 70vh;
+  margin: 40px auto;
+  background-color: rgba(50, 50, 50, 0.5);
+}
+
+div.user-photo {
+  text-align: center;
+  padding-top: 10vh;
+}
+
+div.user-photo > img {
+  border-radius: 50%;
+  width: 20vh;
+}
+
+div.user-username {
+  text-align: center;
+  font-size: 24px;
+  font-weight: 600;
+  color: white;
+  padding-top: 2vh;
+}
+
+div.user-select-bot {
+  padding-top: 20vh;
+}
+
+div.user-select-bot {
+  width: 60%;
+  margin: auto;
+}
+</style>
--- KOB/web/src/views/pk/PkIndexView.vue
+++ KOB/web/src/views/pk/PkIndexView.vue
@@ -1,101 +1,100 @@
 <template>
-    <PlayGround v-if="$store.state.pk.status === 'playing'"></PlayGround>
-    <MatchGround v-if="$store.state.pk.status === 'matching'"></MatchGround>
-    <ResultBoard v-if="$store.state.pk.loser !== 'none'"></ResultBoard>
+  <PlayGround v-if="$store.state.pk.status === 'playing'"></PlayGround>
+  <MatchGround v-if="$store.state.pk.status === 'matching'"></MatchGround>
+  <ResultBoard v-if="$store.state.pk.loser !== 'none'"></ResultBoard>
 </template>
 
 <script>
-import MatchGround from '@/components/MatchGround.vue';
-import PlayGround from '@/components/PlayGround.vue';
-import ResultBoard from '@/components/ResultBoard.vue';
-import { onMounted, onUnmounted } from 'vue';
-import { useStore } from 'vuex';
-
-
+import MatchGround from "@/components/MatchGround.vue";
+import PlayGround from "@/components/PlayGround.vue";
+import ResultBoard from "@/components/ResultBoard.vue";
+import { onMounted, onUnmounted } from "vue";
+import { useStore } from "vuex";
 
 export default {
-    name: 'PkIndexView',
-    components: {
-        PlayGround,
-        MatchGround,
-        ResultBoard,
-    },
-    setup() {
-        const store = useStore();
-        const socketUrl = `ws://localhost:3000/websocket/${store.state.user.token}/`;
+  name: "PkIndexView",
+  components: {
+    PlayGround,
+    MatchGround,
+    ResultBoard,
+  },
+  setup() {
+    const store = useStore();
+    const socketUrl = `ws://localhost:3000/websocket/${store.state.user.token}/`;
 
-        let socket = null;
-        // 当组件被挂载创建链接
-        onMounted(() => {
-            // 设置默认匹配页面
-            store.commit("updateOpponent", {
-                username: "我的对手",
-                photo: require('@/assets/images/pk.png'),
-            });
+    store.commit("updateLoser", "none");
 
-            socket = new WebSocket(socketUrl);  
+    let socket = null;
+    // 当组件被挂载创建链接
+    onMounted(() => {
+      // 设置默认匹配页面
+      store.commit("updateOpponent", {
+        username: "我的对手",
+        photo: require("@/assets/images/pk.png"),
+      });
 
-            // 打开组件时触发
-            socket.onopen = () => {
-                console.log("connected!");
-                store.commit("updateSocket", socket);
-            };
+      socket = new WebSocket(socketUrl);
 
-            // 收到信息时调用
-            socket.onmessage = msg => {
-                // Spring是将数据放入data中
-                const data = JSON.parse(msg.data);
-                // 接受到匹配成功信息后
-                if (data.event === "start-matching") { // 匹配成功
-                    store.commit("updateOpponent", {
-                        username: data.opponent_username,
-                        photo: data.opponent_photo,
-                    });
-                    setTimeout(() => {
-                        // 匹配成功后跳转到游戏页面延迟2秒
-                        store.commit("updateStatus", "playing");
-                    }, 200);
-                    // 接受信息后更新地图
-                    store.commit("updateGame", data.game);
-                } else if (data.event === "move") {
-                    console.log(data);
+      // 打开组件时触发
+      socket.onopen = () => {
+        console.log("connected!");
+        store.commit("updateSocket", socket);
+      };
 
-                    const game = store.state.pk.gameObject;
-                    const [snake0, snake1] = game.snakes;
-                    snake0.set_direction(data.a_direction);
-                    snake1.set_direction(data.b_direction);
-                } else if (data.event === "result") {
-                    console.log(data);
+      // 收到信息时调用
+      socket.onmessage = (msg) => {
+        // Spring是将数据放入data中
+        const data = JSON.parse(msg.data);
+        // 接受到匹配成功信息后
+        if (data.event === "start-matching") {
+          // 匹配成功
+          store.commit("updateOpponent", {
+            username: data.opponent_username,
+            photo: data.opponent_photo,
+          });
+          setTimeout(() => {
+            // 匹配成功后跳转到游戏页面延迟2秒
+            store.commit("updateStatus", "playing");
+          }, 200); // 不知道为什么不能加0
+          // 接受信息后更新地图
+          store.commit("updateGame", data.game);
+        } else if (data.event === "move") {
+          console.log(data);
 
-                    const game = store.state.pk.gameObject;
-                    const [snake0, snake1] = game.snakes;
-                    
-                    if (data.loser === "all" || data.loser === "A") {
-                        snake0.status = "die";
-                    } 
+          const game = store.state.pk.gameObject;
+          const [snake0, snake1] = game.snakes;
+          snake0.set_direction(data.a_direction);
+          snake1.set_direction(data.b_direction);
+        } else if (data.event === "result") {
+          console.log(data);
 
-                    if (data.loser === "all" || data.loser === "B") {
-                        snake1.status = "die";
-                    }
-                    store.commit("updateLoser", data.loser);
-                }
-            }
+          const game = store.state.pk.gameObject;
+          const [snake0, snake1] = game.snakes;
 
-            // 关闭时触发
-            socket.onclose = () => {
-                console.log("disconnected!");
-                store.commit("updateStatus", "matching");
-            }
-        });
-        
-        // 当当前界面被卸载
-        onUnmounted(() => {
-            socket.close();
-        });
-    }
-}
+          if (data.loser === "all" || data.loser === "A") {
+            snake0.status = "die";
+          }
+
+          if (data.loser === "all" || data.loser === "B") {
+            snake1.status = "die";
+          }
+          store.commit("updateLoser", data.loser);
+        }
+      };
+
+      // 关闭时触发
+      socket.onclose = () => {
+        console.log("disconnected!");
+        store.commit("updateStatus", "matching");
+      };
+    });
+
+    // 当当前界面被卸载
+    onUnmounted(() => {
+      socket.close();
+    });
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>

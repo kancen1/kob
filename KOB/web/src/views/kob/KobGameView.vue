1a7c016b6649d438a8626949a318c747c2c6c042
kancen <kancen@github.com>
2026-06-14 17:34:23 +0800

    feat: 两年后用Claude完善项目 - 集成拳皇 + DeepSeek AI
    - 集成kofEnd拳皇对战游戏(iframe方式)
    - 默认AI对手使用DeepSeek大模型+启发式策略
    - 修复蛇头位置bug导致AI只会走一个方向
    - 前端API地址适配本地开发(localhost:3000)
    - 添加MySQL Docker容器支持

--- KOB/web/src/views/kob/KobGameView.vue
+++ KOB/web/src/views/kob/KobGameView.vue
@@ -1,26 +1,29 @@
-<template>  
-  <KobGround>
-    
-  </KobGround>
-  <GameMap>
-    
-  </GameMap>
+<template>
+  <div class="kof-container">
+    <iframe src="/kof.html" class="kof-iframe"></iframe>
+  </div>
 </template>
 
 <script>
-import KobGround from '@/components/KobGround.vue';
-
 export default {
     name: "KobGameView",
-    components: {
-        KobGround,
-    }
 }
 </script>
 
 <style scoped>
-.kob-game-view {
-  text-align: center;
-  font-size: 20px;
+.kof-container {
+  position: fixed;
+  top: 56px;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow: hidden;
+  background: #000;
+}
+.kof-iframe {
+  width: 100%;
+  height: 100%;
+  border: none;
+  outline: none;
 }
 </style>
